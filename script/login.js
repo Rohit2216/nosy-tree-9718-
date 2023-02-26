2e101e6c338d463fb0783b9eb7460d049c2bfbf3
Rohit2216 <chauhanrohit716@gmail.com>
2023-02-26 21:19:55 +0530

    all done

--- script/login.js
+++ script/login.js
@@ -7,10 +7,14 @@ loginBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let userMobile = form.mobile.value;
     let password = form.password.value;
-    if (userMobile === getData.mobileNum && password === getData.password)
+    if (userMobile==="1234567890" && password==="admin@123"){
+        alert("Your are admin")
+        window.location.href='admin.html'
+    }
+    else if (userMobile === getData.mobileNum && password === getData.password)
     {
         alert("Login Successfully");
-        window.location.href = 'h.html'
+        window.location.href = 'index.html'
     }
 
     else
